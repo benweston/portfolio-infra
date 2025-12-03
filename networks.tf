3f6f7467579e0ec45697b6b56ab39f232b4ead42
Ben Weston <westonb26918@proton.me>
2025-12-03 09:45:41 +0000

    Update CIDR range for networks.tf

--- networks.tf
+++ networks.tf
@@ -2,5 +2,5 @@ resource "digitalocean_vpc" "portfolio_infra" {
   name        = "portfolio-infra"
   region      = "lon1"
   description = "Portfolio VPC."
-  ip_range    = "10.30.0.0/20"
+  ip_range    = "10.40.0.0/20"
 }
